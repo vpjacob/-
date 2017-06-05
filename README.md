14733c58c6f1d19233d30848035323df9f64f2bc
vpjacob <824629419@qq.com>
2017-06-05 17:11:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# -
+![](file:////Users/vpjacob/Desktop/Demo/生成二维码图片/Snip20170605_3.png) 
+
+
+![](file:////Users/vpjacob/Desktop/Demo/生成二维码图片/Snip20170605_4.png) 
---
+++ 生成二维码图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  生成二维码图片
+//
+//  Created by vpjacob on 2017/6/5.
+//  Copyright © 2017年 vpjacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
